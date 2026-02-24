c794bb3aca2fe43f07e7b7380e150dde6f801dbd
mgianzantti <eduardagianzantti@gmail.com>
2026-02-24 11:18:54 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App () {
+  return(
+    <div>
+      <h1>Aula de React com Vite JS</h1>
+      <h3>Estilização CSS</h3>
+      <p>Eu não fiz nada</p>
+    </div>
+  );
+}
+
+export default App;
