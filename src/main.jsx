35255661f095f3348efe87be2de373a51980559e
mgianzantti <eduardagianzantti@gmail.com>
2026-02-24 11:45:06 -0300

    ajuste css

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './reset.css' 
-import './global.css'
+import './css/reset.css' 
+import './css/global.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
