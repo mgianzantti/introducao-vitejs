8c6d27b2be5f2763e7afc05945de521604d81314
mgianzantti <eduardagianzantti@gmail.com>
2026-02-27 14:38:09 -0300

    aula7-02

---
+++ src/sobre/page.jsx
@@ -0,0 +1,10 @@
+function Sobre () {
+    return(
+        <div>
+             <h1>Sobre</h1>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure error, ad fuga eveniet cumque mollitia culpa eos obcaecati explicabo, perferendis esse, modi voluptatibus et voluptatum? Consectetur architecto debitis veritatis eaque?</p>
+        </div>
+    )
+}
+
+export default Sobre; 
