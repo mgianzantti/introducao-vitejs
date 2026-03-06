8bdf957a17ea29ce2a8c350c5fd7cde7580fda09
mgianzantti <eduardagianzantti@gmail.com>
2026-03-06 14:42:08 -0300

    aula0603

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,8 +3,6 @@ function Home () {
     return(
         <div>
              <h1>Aula de React com Vite JS</h1>
-             <h2>Estilização CSS</h2>
-             <p>Eu não fiz nada</p>
         </div>
     )
 }
