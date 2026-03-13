c4feb84c83754752c07f186e8e8554f4376c40ef
mgianzantti <eduardagianzantti@gmail.com>
2026-03-13 14:40:24 -0300

    aula1303

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,25 @@
 
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router";
+import './index.css'
+
 function Home () {
+    
+    const navigate = useNavigate ();
+
     return(
-        <div>
+        <div className='containerHome'>
+            <div className="containerCards"></div>
              <h1>Aula de React com Vite JS</h1>
+
+             <button
+                onClick={() => navigate('/exemplo/1')}
+                >
+              Exemplo 1
+             </button>
+
+             <Link to="exemplo/2">Exemplo 2</Link>
+
         </div>
     )
 }
