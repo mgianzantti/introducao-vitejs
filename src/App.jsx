3e32bfed87339f9f87bfc5ba266b4f419ab9c572
mgianzantti <eduardagianzantti@gmail.com>
2026-03-24 11:47:28 -0300

    aula2403

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom"
 import Home from './pages/home'
 import Exemplo01 from "./pages/exemplos/ex-01";
 import Exemplo02 from "./pages/exemplos/ex-02";
+import Exemplo03 from "./pages/exemplos/ex-03";
 import Atividade1 from "./pages/atividades/atv-01";
 
 function App () {
@@ -12,6 +13,7 @@ function App () {
     <Route path="/" element={<Home />} />
     <Route path="/exemplo/1" element={<Exemplo01 />} />
     <Route path="/exemplo/2" element={<Exemplo02 />} />
+    <Route path="/exemplo/3" element={<Exemplo03/>}/>
        <Route path="/atividades/" element={<Atividade1 />} />
     </Routes>
  )
